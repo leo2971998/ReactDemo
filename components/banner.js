19706a5821fc70bb92f9a9413138eee5d0140907
leo2971998 <nanhtu297@gmail.com>
2022-11-04 13:33:11 -0500

    Init

---
+++ components/banner.js
@@ -0,0 +1,13 @@
+const Banner = () => {
+  return (
+      <header>
+          <div>
+              <img src = "../vercel.svg" alt = "logo"/>
+          </div>
+          <div>
+              Providing houses all over the world
+          </div>
+      </header>
+  );
+};
+export default Banner;
